c99a74eb00f9067d8577d8ff235ed18af9a07b7a
Ozgur Adem Isikli <i.ozguradem@gmail.com>
2023-03-03 21:16:33 +0100

    Fixed integration tests

--- tests/integrations/scenarios/app/v1/config.ts
+++ tests/integrations/scenarios/app/v1/config.ts
@@ -1,10 +1,18 @@
-import { IVersionConfig } from "axe-api";
+import { IVersionConfig, allow, QueryFeature } from "axe-api";
 
 const config: IVersionConfig = {
   transaction: [],
   serializers: [],
   supportedLanguages: ["en", "de"],
   defaultLanguage: "en",
+  query: {
+    limits: [
+      allow(QueryFeature.FieldsAll),
+      allow(QueryFeature.WithHasOne),
+      allow(QueryFeature.Sorting),
+      allow(QueryFeature.WhereAll),
+    ],
+  },
 };
 
 export default config;
